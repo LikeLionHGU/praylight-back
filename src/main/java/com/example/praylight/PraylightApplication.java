c3c514d39e663a5919be57209c0dfeaa3e8000be
blackcowman <soh990301@gmail.com>
2023-11-01 22:12:19 +0900

    init : 프로젝트 시작!!

---
+++ src/main/java/com/example/praylight/PraylightApplication.java
@@ -0,0 +1,13 @@
+package com.example.praylight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraylightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PraylightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/praylight/PraylightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.praylight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraylightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
